4d51f75ab161f20cdc5eadca4eb37f469020752f
Parth Maheshwari <148962439+Parth-github3@users.noreply.github.com>
2024-10-10 11:47:05 +0530

    Update Assistant.py

--- Assistant.py
+++ Assistant.py
@@ -22,21 +22,16 @@ Provide answers for the following list of questions with explapaination and also
     | StrOutputParser()
     |{"base_response": RunnablePassthrough()}
     )
-    res =base_summary_chain.invoke(userinput)
-    return res
+    return base_summary_chain
+    
+    
 
 
 # Title of the app
 st.title("AI by PARTH")
 
 # Introduction text
-with st.expander("About app..."):
-    st.write("""
-        Welcome to your Personal Health Partner (PHP) ! 
-        My AI-powered (by LLaMA 3 70B) app is designed to provide you with reliable and accessible health information at your fingertips.\n
-        This app compiles every field of your life, you want guidance for. Areas such as Health- Mental, Physical are coverd.\n
-        Whether you have a specific health query or simply want to learn more about wellness,  PHP is here to assist you.\n
-    """)
+
 
 # Sidebar for additional information
 
@@ -50,12 +45,12 @@ with st.expander("About app..."):
 # Load Groq compiled LLaMA model (replace with your actual model path)
 @st.cache_resource
 
-def yoo():
-     var= ""
-
+def generate_response(userinput):
+     res = Assistant.invoke(userinput)
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+# getting User input
 userinput = st.chat_input("Say something")
 with st.chat_message("user"):
         st.write(userinput)
@@ -64,17 +59,8 @@ if userinput:
     message = st.chat_message("assistant")
     #message.write(cbt_chain.invoke(user_input))
     st.session_state.messages.append({"role": "user", "content": userinput})
-    bot_response = Assistant(userinput)
+    bot_response = generate_response(userinput)
     st.session_state.messages.append({"role": "assistant", "content": bot_response})
-
-# with st.chat_message:
-#     user_input = st.text_input("You: ", key="user_input", help="Type your message and press Enter")
-
-# If user has input a message, update the chat history and get response
-# if user_input:
-#     st.session_state.messages.append({"role": "user", "content": user_input})
-#     bot_response = cbt_chain.invoke(user_input)
-#     st.session_state.messages.append({"role": "assistant", "content": bot_response})
 
 # Display chat history
 for message in st.session_state.messages:
