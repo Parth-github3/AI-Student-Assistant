5d3c5a9282fd5a30896b75180fd7cd7a5335113c
Parth Maheshwari <148962439+Parth-github3@users.noreply.github.com>
2024-10-11 14:06:26 +0530

    Update Assistant.py

--- Assistant.py
+++ Assistant.py
@@ -190,11 +190,7 @@ for uploaded_file in uploaded_files:
             for question in questions:
         #aprompt = answer_prompt_template.format(question=question)
                 answers = aprompt.invoke(question)
- 
-                    
-            if st.button("Submit"):
-                    response =answers
-                    st.text_area("content:", value=response, height=300)
+                st.text_area("content:", value=response, height=300)
 # getting User input
 # userinput = st.chat_input("Say something")
 # with st.chat_message("user"):
