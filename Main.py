6574ed10fa84c5213db78c2b56f4d28fb31e1bf3
Parth Maheshwari <148962439+Parth-github3@users.noreply.github.com>
2024-10-14 20:51:22 +0530

    f

--- Main.py
+++ Main.py
@@ -19,14 +19,6 @@ pages = {
 if st.sidebar.button("Clear History"):
     st.session_state.clear()
 
-class info:
 
-        if pages == "Question":
-            qinfo = "Enter pdf"
-            st.sidebar.markdown(qinfo)
-        elif pages == "Answer":
-            ainfo = "Enter text ile"
-            st.sidebar.markdown(ainfo)
-st.sidebar.markdown("hi")
 pg = st.navigation(pages)
 pg.run()
