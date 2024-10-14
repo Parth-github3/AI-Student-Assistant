66147647bd083c6456e041572e46318d752c2c98
Parth Maheshwari <148962439+Parth-github3@users.noreply.github.com>
2024-10-14 12:16:16 +0530

    Update Assistant.py

--- Assistant.py
+++ Assistant.py
@@ -101,7 +101,7 @@ qchain= ( ChatPromptTemplate.from_template("Provide a list of the repeated quest
                       #| {"q_response": RunnablePassthrough()}
                       
             )
-achain= ( ChatPromptTemplate.from_template("You are an intelligent ai which generates informative answers for all the questions given to you {base_response} with Explaination of every answer in detail. ")
+achain= ( ChatPromptTemplate.from_template("Assume that you are writing anwers in an exam for the following questions: {base_response}.")
                       | llama
                       | StrOutputParser()
             )
