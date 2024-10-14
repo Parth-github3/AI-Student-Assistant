0ae78cd0522d154f7df7a62ede7f7e31e5041d5e
Parth Maheshwari <148962439+Parth-github3@users.noreply.github.com>
2024-10-14 11:58:10 +0530

    Update Assistant.py

--- Assistant.py
+++ Assistant.py
@@ -135,7 +135,7 @@ mainchain = (
             | responderchain
             )
 demchain = (
-     qchain
+     basechain
      | achain
 
      
@@ -235,7 +235,7 @@ if st.button("submit"):
      message = st.chat_message("assistant")
      #message.write(cbt_chain.invoke(user_input))
      #st.session_state.messages.append({"role": "user", "content":})
-     bot_response = achain.invoke(res)
+     bot_response = demchain.invoke(res)
      
      st.session_state.messages.append({"role": "assistant", "content": bot_response})
 
