0cfdaf3aeabb0eb7a869b6a6680bb98f25f229b0
Parth Maheshwari <148962439+Parth-github3@users.noreply.github.com>
2024-10-14 11:56:56 +0530

    Update Assistant.py

--- Assistant.py
+++ Assistant.py
@@ -235,7 +235,7 @@ if st.button("submit"):
      message = st.chat_message("assistant")
      #message.write(cbt_chain.invoke(user_input))
      #st.session_state.messages.append({"role": "user", "content":})
-     bot_response = responderchain.invoke(res)
+     bot_response = achain.invoke(res)
      
      st.session_state.messages.append({"role": "assistant", "content": bot_response})
 
