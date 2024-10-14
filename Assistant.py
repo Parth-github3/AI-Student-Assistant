7b78f3611357798cb3acd93cf5f033e6546865d1
Parth Maheshwari <148962439+Parth-github3@users.noreply.github.com>
2024-10-14 10:46:28 +0530

    Update Assistant.py

--- Assistant.py
+++ Assistant.py
@@ -224,7 +224,8 @@ if st.button("submit"):
      message = st.chat_message("assistant")
      #message.write(cbt_chain.invoke(user_input))
      #st.session_state.messages.append({"role": "user", "content":})
-     bot_response = demchain.invoke(res)
+     #bot_response = demchain.invoke(res)
+     st.write(res)
      st.session_state.messages.append({"role": "assistant", "content": bot_response})
 
 # getting User input
