5d07eff93d8ba2e2e768588e4beeac20222bca61
Parth Maheshwari <148962439+Parth-github3@users.noreply.github.com>
2024-10-15 09:10:31 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,7 +8,9 @@ This app will make your exam preparations smooth as butter!\n
             2. Answer
             3. Concept-Learning Plan
 """)
-st.markdown("***How to use?***")
+st.markdown("**How to use?**")
+with st.expander("About Question..."):
+    st.markdown("Open the Question page -> Upload pyq papers -> Download the ***Question_response*** text file.\n You can you this file for both Answer page and Concept-Learning Plan for getting responses.")
 with st.expander("About Question..."):
     st.markdown("""
     "Question" is a place where you upload your Past Year Question papers and get your imp questions in seconds.\n
